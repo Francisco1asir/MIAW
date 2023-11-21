7f1f401cfad16208b284d665284023b06f214e08
Francisco1asir <fparnav1908@g.educaand.es>
2023-11-21 13:20:55 +0100

    Borrado final

--- api-libreria2/src/modules/seed/seed.service.ts
+++ api-libreria2/src/modules/seed/seed.service.ts
@@ -4,27 +4,27 @@ import * as seedLibros from '../seed/data/libros.json'
 import { AutoresService } from '../autores/autores.service';
 import { Autor } from '../autores/interfaces/autor.interface';
 
+
 @Injectable()
 export class SeedService {
-  constructor (private readonly autoreService: AutoresService){}
-
+  constructor (
+    private readonly autoreService: AutoresService,
+    ){}
+  
   public loadData(){
-    seedAutores.forEach((autor:Autor) => {
-      console.log(autor.nombre);
-      this.autoreService.create(autor);
-    })
-    return {
-      msg: 'Carga de datos finalizada'
-    }
+    this.insertNewAutores();
   }
 
   private async insertNewAutores(){
     await this.autoreService.deleteAllAutores();
     const insertPromisesAutores = [];
-    seedAutores.forEach((autor:Autor) => {
-      insertPromisesAutores.push(this.autoreService.create(autor))
-    });
-    const result = await Promise.all(insertPromisesAutores);
-    return true;
+    seedAutores.forEach( (autor: Autor) => {
+    insertPromisesAutores.push(this.autoreService.create(autor));
+  })
+  const results = await Promise.all(insertPromisesAutores);
+  return true
   }
-}
+
+
+  }
+  
